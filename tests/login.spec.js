a9eb28865ec8c9d00d74af259ee1ca98407f965e
BlineraHysenaj <blinera.hysenaj@gmail.com>
2024-10-13 20:13:49 +0200

    Removing unnecessary tests

--- tests/login.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test')
-import { LoginPage } from '../pages/login'
-
-test('Login Test', async ({ page }) => {
-    const Login = new LoginPage(page)
-    await Login.goToSauceDemo()
-    await Login.login('standard_user', 'secret_sauce')
-})
