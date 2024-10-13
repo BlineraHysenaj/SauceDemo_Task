835e69f0e852defcb9165e44824bf4ea624376de
BlineraHysenaj <blinera.hysenaj@gmail.com>
2024-10-13 20:18:52 +0200

    Update the naming of selectors

--- pages/logout.js
+++ pages/logout.js
@@ -1,22 +1,22 @@
 exports.LogoutPage = class LogoutPage {
     constructor(page) {
         this.page = page;
-        this.burgerMenuButton = '#react-burger-menu-btn'; 
-        this.logoutButton = '#logout_sidebar_link'; 
+        this.burger_menu_button = '#react-burger-menu-btn'; 
+        this.logout_button = '#logout_sidebar_link'; 
     }
   
     async performLogout() {
-        await this.page.click(this.logoutButton);
+        await this.page.click(this.logout_button);
         await this.page.waitForURL('**/login');
     }
     async clickBurgerMenu() {
-        await this.page.waitForSelector(this.burgerMenuButton);
-        await this.page.click(this.burgerMenuButton);
+        await this.page.waitForSelector(this.burger_menu_button);
+        await this.page.click(this.burger_menu_button);
       }
     
       async clickLogout() {
-        await this.page.waitForSelector(this.logoutButton);
-        await this.page.click(this.logoutButton);
+        await this.page.waitForSelector(this.logout_button);
+        await this.page.click(this.logout_button);
       }
   };
   
