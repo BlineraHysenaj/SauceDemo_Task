de9b4014a46ff5cdd6e6151b56afa807a74ea412
BlineraHysenaj <blinera.hysenaj@gmail.com>
2024-10-13 22:27:52 +0200

    Updated Remove Item from Cart

--- pages/homePage.js
+++ pages/homePage.js
@@ -4,18 +4,25 @@ exports.HomePage = class HomePage {
     this.product_list =
       '//*[@id="inventory_container"]/div/div/div/div/div/div/a';
     this.add_to_cart = '[data-test="add-to-cart"]';
-    this.remove_from_cart = '[data-test="remove"]';
+    this.remove_from_cart = '[data-test="remove-sauce-labs-backpack"]';
     this.cart = '[data-test="shopping-cart-link"]';
   }
 
   async addProductToCart(productName) {
     const product_list = await this.page.$$(this.product_list);
+
     for (const product of product_list) {
       const productTitle = await product.textContent();
+
       if (productTitle.trim() === productName) {
         await product.click();
         await this.page.locator(this.add_to_cart).click();
+
+        await this.goToCart();
+
         await this.page.locator(this.remove_from_cart).click();
+        await this.page.goBack();
+
         await this.page.locator(this.add_to_cart).click();
         break;
       }
